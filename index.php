8f9766e16a2999c4d425f566d4bdfa600e475c3c
mmva <mmva@cta.es>
2012-11-26 13:12:43 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?PHP
+echo "Hola mundo";
+// phpinfo();
+print("<p>Párrafo 1</p>\n");
+print("<p>Párrafo 2</p>\n");
+
+echo "<br>";
+include("../libs/incluyeme.php");
+
+echo "<br>";
+require_once("variables.php");
+
+echo "<br>Esto es lo siguiente";
+?>
+
